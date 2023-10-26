89ae3b65f6a227996cb64d358cb8080b12fd73ec
Dagadev <gacub20ru@gmail.com>
2023-10-25 20:59:19 -0600

    implement currency convert feching

--- src/components/calculator/table.tsx
+++ src/components/calculator/table.tsx
@@ -1,5 +1,7 @@
+import { useState, useEffect } from 'react'
 import useCalculator from '@/hooks/use-calculatortools'
 import TableRow from './table-row'
+import { customApiFetch } from '@/utils/servicestools'
 
 const REWARDS_ROWS = [
   {
@@ -25,22 +27,38 @@ const REWARDS_ROWS = [
 ]
 
 export default function Table () {
-  const { blockReward, blockTime } = useCalculator()
+  const { currency } = useCalculator()
+  const [USDTValue, setUSDTValue] = useState(0)
+
+  useEffect(() => {
+    if (currency !== undefined && ['RLT', 'RST'].every(e => e !== currency.name)) {
+      customApiFetch<{
+        success: boolean
+        error: string
+        value: number
+      }>(`currency/convert/${currency.code}/usdt/1`).then(data => {
+        // console.log(data)
+        if (data.success) {
+          setUSDTValue(data.value)
+        }
+      })
+    }
+  }, [currency])
 
   return (
     <section className='p-4 rounded-lg shadow-lg border border-slate-700 bg-slate-800'>
-      <h2>Results table</h2>
+      <h2 className='text-xl font-bold mb-3'>Results table</h2>
 
-      <table className='border-collapse'>
+      <table className='rounded-md border-collapse bg-gray-900'>
         <thead>
-          <tr>
-            <th className='border px-3 py-2 text-left'>Reward by</th>
-            <th className='border px-3 py-2 text-left'>Reward</th>
-            <th className='border px-3 py-2 text-left'>USDT</th>
+          <tr className='border border-gray-600 bg-gray-800'>
+            <th className='px-3 py-2 text-left'>Reward by</th>
+            <th className='px-3 py-2 text-left'>Reward</th>
+            <th className='px-3 py-2 text-left'>USDT</th>
           </tr>
         </thead>
         <tbody>
-          {REWARDS_ROWS.map(rr => <TableRow key={rr.name} rowData={rr} {...{ blockReward, blockTime }} />)}
+          {REWARDS_ROWS.map(rr => <TableRow key={rr.name} rowData={rr} USDTValue={USDTValue} />)}
         </tbody>
       </table>
     </section>
