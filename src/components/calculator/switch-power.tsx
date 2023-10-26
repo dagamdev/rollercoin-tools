f924bcd124c7fcd19d1f40b73ec5549e2b066466
Dagadev <gacub20ru@gmail.com>
2023-10-25 20:57:27 -0600

    update styles

--- src/components/calculator/switch-power.tsx
+++ src/components/calculator/switch-power.tsx
@@ -9,16 +9,19 @@ export default function SwitchPower ({ addPower, setAddPower }: {
   }
 
   return (
-    <section className='flex flex-col items-center'>
-      <div className={'flex w-[50px] rounded-xl border cursor-pointer relative border-gray-700 bg-gray-900'}
-        onClick={handleClick}
-      >
-        <span className={'block h-[22px] w-[22px] rounded-full relative transition-all duration-300' + (addPower
-          ? ' left-[26px] bg-green-500'
-          : ' left-0 bg-gray-600'
-        )}></span>
-      </div>
+    <section className='flex flex-col'>
       <p>Addition power</p>
+      <div className='flex flex-1 items-center'>
+        <div className={'flex mt-1 w-[50px] rounded-xl border cursor-pointer relative border-gray-700 bg-gray-900'}
+          onClick={handleClick}
+        >
+          <span className={'block h-[22px] w-[22px] rounded-full relative transition-all duration-300' + (addPower
+            ? ' left-[26px] bg-green-500'
+            : ' left-0 bg-gray-600'
+          )}></span>
+        </div>
+
+      </div>
     </section>
   )
 }
