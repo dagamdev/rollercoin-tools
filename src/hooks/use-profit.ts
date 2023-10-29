f7f3b9b8dc88e2ac84c970a5a006343f4e22bac3
Dagadev <gacub20ru@gmail.com>
2023-10-28 19:29:08 -0600

    update custom hook

--- src/hooks/use-profit.ts
+++ src/hooks/use-profit.ts
@@ -1,6 +1,6 @@
 import { useContext } from 'react'
 import { ProfitContext, type ProfitContextData } from '@/contexts/profit-contexttools'
 
-export default function useCalculator () {
+export default function useProfit () {
   return useContext(ProfitContext) as ProfitContextData
 }
