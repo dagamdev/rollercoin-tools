5086b5de88442ddae3e520fa0a884c0fb9da4025
Dagadev <gacub20ru@gmail.com>
2023-11-17 20:26:09 -0600

    add main page content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home () {
   return (
     <>
-      <h1 className='text-3xl font-bold'>Roller tools</h1>
+      <h1 className='text-3xl font-bold mb-5'>Roller tools</h1>
+
+      <p className='max-w-lg'>Herramientas para <strong><a className='underline hover:no-underline text-blue-500' href='https://rollercoin.com' target='_blank' rel="noreferrer">🐹 RollerCoin</a></strong> como la calculadora de <strong>ganancias</strong> para decidir qué moneda minar, la calculadora de <strong>rentabilidad</strong> para saber si una nueva adquisición es rentable en base a su costo por <strong>RLT</strong> y las ganancias de poder y bonus.</p>
     </>
   )
 }
