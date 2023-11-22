467ecb810a7c7e7e69978a81042105f745513492
Dagadev <gacub20ru@gmail.com>
2023-11-22 12:57:07 -0600

    update header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -25,14 +25,14 @@ export default function Header () {
   const pathName = usePathname()
 
   return (
-    <header className='flex justify-center p-5 shadow-lg bg-slate-800'>
+    <header className='flex justify-center p-4 shadow-lg bg-slate-800'>
       <nav>
         <ul className='flex items-end gap-4'>
           {PAGES_PATHS.map(pp => <li key={pp.path}>
             <Link className={`key-button ${pathName === pp.path ? 'pressed ' : 'hover:brightness-110 '}flex flex-col mb-[4px] rounded-lg bg-amber-700`}
               href={pp.path}
             >
-              <span className='flex py-2 px-5 rounded-lg text-2xl border bg-amber-500 border-amber-400'>{pp.icon}</span>
+              <span className='flex py-2 px-5 rounded-lg text-xl border bg-amber-500 border-amber-400'>{pp.icon}</span>
               <div className='h-[8px] rounded-b-lg' />
             </Link>
           </li>)}
